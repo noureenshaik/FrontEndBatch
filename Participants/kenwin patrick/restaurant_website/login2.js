e1dd0f2145478f50014818f4c9165456a3058f5b
Kenwin-Disys <124788899+Kenwin-Disys@users.noreply.github.com>
2023-02-13 18:04:46 +0530

    Add files via upload

---
+++ Participants/kenwin patrick/restaurant_website/login2.js
@@ -0,0 +1,16 @@
+function validate()
+{
+var username=document.getElementById("username").value;
+var password=document.getElementById("password").value;
+
+if (username==null || username==""){  
+    alert("Name can't be blank");  
+    return false;  
+  }else if(password.length<6){  
+    alert("Password must be at least 6 characters long.");  
+    return false;  
+    } 
+
+}
+
+
