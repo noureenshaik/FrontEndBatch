26b2adb4b6b2356dff334dfef30f459f67d535ef
Sainikhil-disys <124788720+Sainikhil-disys@users.noreply.github.com>
2023-02-14 14:53:09 +0530

    Add files via upload

---
+++ Participants/sai nikhil perumalla/Hands On Work/js.js
@@ -0,0 +1,13 @@
+<html>
+    <body>
+        <p> I Am learning Java Script Toady</p>
+        <p id="demo"></p>
+        <script>
+            a=10;
+            b=20;
+            c=a+b;
+            document.getElementById=
+            
+        </script>
+    </body>
+</html>
